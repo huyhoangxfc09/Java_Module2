3b9516f10f25ca52b403df84869bdbe311f0b62c
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-01 23:56:06 +0700

    Update currencyConversion.java

--- IntroductionToJava/src/currencyConversion.java
+++ IntroductionToJava/src/currencyConversion.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class currencyConversion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập tiền usd:\n");
+        System.out.print("Input USD:\n");
         int usd = scanner.nextInt();
         final int RATE = 23000;
         int vnd = usd*RATE;
