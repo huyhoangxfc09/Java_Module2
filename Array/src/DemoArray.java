371ae8228b6975053cd492ecf12696d9c58a00c8
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-04 22:47:27 +0700

    Create DemoArray.java

---
+++ Array/src/DemoArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DemoArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập kích thước của mảng: ");
+        int size = scanner.nextInt();
+        int[] Array = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Nhập phần tử thứ " + (i + 1));
+            Array[i] = scanner.nextInt();
+        }
+        System.out.println("Mảng tạo mới là:");
+        for (int element : Array) {
+            System.out.print(element + " ");
+        }
+    }
+}
