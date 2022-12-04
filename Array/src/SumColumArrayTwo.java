d4eb9903ab5fbfd065d6e60ec296ffbea4f837c8
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-04 23:09:19 +0700

    Update SumColumArrayTwo.java

--- Array/src/SumColumArrayTwo.java
+++ Array/src/SumColumArrayTwo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class SumColumArrayTwo {
     public static void main(String[] args) {
-        Scanner scanner =new  Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập số dòng:");
         int row = scanner.nextInt();
         System.out.println("Nhập số cột:");
@@ -11,12 +11,12 @@ public class SumColumArrayTwo {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < colum; j++) {
                 System.out.println("Nhập phần tử thứ [" + i + ", " + j + "]: ");
-                array[i][j]= scanner.nextInt();
+                array[i][j] = scanner.nextInt();
             }
         }
         System.out.println("Mảng hai chiều tạo ra là");
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j <colum; j++) {
+            for (int j = 0; j < colum; j++) {
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println("\n");
@@ -24,9 +24,13 @@ public class SumColumArrayTwo {
         System.out.println("Nhập cột cần tính tổng:");
         int index = scanner.nextInt();
         int sum = 0;
-        for (int i = 0; i < row; i++) {
-                sum+=array[i][index-1];
+        if (colum < index) {
+            System.out.println("Quá số cột hãy nhập lại.");
+        } else {
+            for (int i = 0; i < row; i++) {
+                sum += array[i][index - 1];
+            }
+            System.out.println("Tổng cột " + index + " là: " + sum);
         }
-        System.out.println("Tổng cột "+index+" là: "+sum);
     }
 }
