475cd90285dcdb97167c2445f5cb8b5627b34366
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-04 23:50:04 +0700

    Create DemKyTuTrongChuoi.java

---
+++ Array/src/DemKyTuTrongChuoi.java
@@ -0,0 +1,27 @@
+public class DemKyTuTrongChuoi {
+    static void DemKyTu(String str)
+    {
+        int counter[] = new int[256];
+        int len = str.length();//Tổng ký tự của chuỗi.
+        for (int i = 0; i < len; i++)
+            counter[str.charAt(i)]++;
+        char array[] = new char[str.length()];
+        for (int i = 0; i < len; i++) {
+            array[i] = str.charAt(i);
+            int flag = 0;
+            for (int j = 0; j <= i; j++) {
+                if (str.charAt(i) == array[j])
+                    flag++;
+            }
+            if (flag == 1)
+                System.out.println("Số lần xuất hiện của " + str.charAt(i)
+                        + " trong chuỗi:" + counter[str.charAt(i)]);
+        }
+    }
+    public static void main(String[] args)
+    {
+        String str = "Tran Huy Hoang";
+        System.out.println("Chuỗi là: "+ str);
+        DemKyTu(str);
+    }
+}
