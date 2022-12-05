41e74988d32ac9e3eace049a782edc7860d1fa76
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-05 14:16:37 +0700

    Create DemoNextLine.java

---
+++ Demo/src/DemoNextLine.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DemoNextLine {
+    public static void main(String[] args) {
+      class Person{
+          public Person(){
+
+          }
+      }
+    }
+}
