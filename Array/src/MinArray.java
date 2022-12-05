9596d4f728a40c2ee8213699035faaa75f923b19
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-05 16:09:25 +0700

    Update MinArray.java

--- Array/src/MinArray.java
+++ Array/src/MinArray.java
@@ -3,24 +3,26 @@ import java.util.Scanner;
 public class MinArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập kích thước của mảng: ");
+        System.out.println("Input size array");
         int size = scanner.nextInt();
-        int[] Array = new int[size];
+        int[] array = new int[size];
         for (int i = 0; i < size; i++) {
-            System.out.println("Nhập phần tử thứ " + (i + 1));
-            Array[i] = scanner.nextInt();
+            System.out.println("Input element "+(i+1)+" : ");
+            array[i]= scanner.nextInt();
         }
-        System.out.println("Mảng tạo mới là:");
-        for (int element : Array) {
-            System.out.print(element + " ");
+        System.out.println("Creat new array:");
+        for (int element : array) {
+            System.out.println(element+" ");
         }
-        System.out.println("\n");
-        int min = Array[0];
-        for (int i = 0; i < size; i++) {
-            if(min>Array[i]){
-                min = Array[i];
+        System.out.println("Minimum value in array: "+minArray(array));
+    }
+    public static int minArray(int[]array){
+        int min = array[0];
+        for (int i = 1; i <array.length ; i++) {
+            if (min>array[i]){
+                min = array[i];
             }
         }
-        System.out.println("Giá trị nhỏ nhất trong mảng là: "+min);
+        return min;
     }
 }
