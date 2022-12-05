10bea835b843d5f1e9d90f8aa9b052d74cc69052
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-05 16:09:21 +0700

    Update CreatArray.java

--- Array/src/CreatArray.java
+++ Array/src/CreatArray.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class CreatArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhập kích thước của mảng: ");
+        System.out.println("Input size array");
         int size = scanner.nextInt();
-        int[] Array = new int[size];
+        int[] array = new int[size];
         for (int i = 0; i < size; i++) {
-            System.out.println("Nhập phần tử thứ " + (i + 1));
-            Array[i] = scanner.nextInt();
+            System.out.println("Input element "+(i+1)+" : ");
+            array[i]= scanner.nextInt();
         }
-        System.out.println("Mảng tạo mới là:");
-        for (int element : Array) {
-            System.out.print(element + " ");
+        System.out.println("Creat new array:");
+        for (int element : array) {
+            System.out.println(element+" ");
         }
     }
 }
