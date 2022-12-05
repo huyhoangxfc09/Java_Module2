526e4118a8fb7546c93df4c158678b96e42b6691
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-05 18:41:55 +0700

    add

--- LoopJava/src/Display20NumberPrime.java
+++ LoopJava/src/Display20NumberPrime.java
@@ -1,27 +1,26 @@
-import java.util.Scanner;
+
 
 public class Display20NumberPrime {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input number:");
-        int number = scanner.nextInt();
         int count = 0;
-        int N = 2;
-        int tempt =0;
-        while (count!=number){
-            for (int i = 2 ; i <=N ; i++) {
-                for (int j = 1; j <=N ; j++) {
-                    if(N%j==0){
-                        tempt+=1;
-                    }
-                }
-                if (tempt==2){
-                    count+=1;
-                    System.out.println("Số thứ "+count+" = "+N);
+        int number =0;
+        while (count<20){
+            if(checkPrime(number)){
+                count++;
+                System.out.println("Element "+count+" => "+number);
+            }
+            number++;
+        }
+    }
+    public static boolean checkPrime(int number){
+        if (number<2){
+            return false;
+        }else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
                 }
             }
-            tempt=0;
-            N+=1;
-        }
+        }return true;
     }
 }
