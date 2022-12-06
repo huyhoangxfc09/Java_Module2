de91d76db5e29354a95d2f5c3d805efdedbfe4a0
huyhoangxfc09 <117245462+huyhoangxfc09@users.noreply.github.com>
2022-12-06 16:55:39 +0700

    add

--- Array/src/DeleteArray.java
+++ Array/src/DeleteArray.java
@@ -20,6 +20,9 @@ public class DeleteArray {
         for (int i = index; i < arr.length - 1; i++) {
             arr[i] = arr[i + 1];
         }
+        arr[arr.length-1]=0;
+        System.out.println("Mảng sau khi xóa là:");
+
         System.out.println("Mảng sau khi xóa là:");
         for (int element : arr) {
             System.out.print(element + " ");
